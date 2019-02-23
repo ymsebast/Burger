edbc10437b96d827d0b309bdf7ac4ee338c4eaf2
ymsebast <ymsebast@uci.edu>
2019-02-23 12:32:28 -0800

    fix server routes

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-// =============================================================
 var express = require("express");
-var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+var PORT = process.env.PORT || 8080;
+
 var app = express();
-var PORT = process.env.port || 3000;
 
-// Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//data
-var friends = require("./app/data/friends");
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//html routing
-var htmlroutes = require("./app/routing/htmlRoutes");
-app.use('/',htmlroutes);
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-//api routing
-var apiroutes = require("./app/routing/apiRoutes");
-app.use('/',apiroutes); 
+app.use(routes);
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORTS " + PORT);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
